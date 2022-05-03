2bc18f19faf907bcbe89345b0677ea3a48deff91
tobiasvinther <tobi626i@stud.kea.dk>
2022-05-03 10:53:14 +0200

    More work on styling the table

--- pages/products/products.js
+++ pages/products/products.js
@@ -1,6 +1,6 @@
 
 
-const headers = ["Mandag\n2/5", 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag'];
+const headers = ["Mandag", 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lørdag', 'Søndag'];
 const dummyTimes = ['10:00', '12:00', '14:00']
 
 
@@ -9,6 +9,8 @@ export function makeTable() {
     let array = dummyTimes
 
     const table = document.getElementById('movie-table-id');
+    const tableHeader = document.getElementById('movie-header-id');
+    const tableBody = document.getElementById('movie-body-id');
 
     //let existingRowsToRemove = document.querySelectorAll('tr');
     //existingRowsToRemove.forEach(row => row.remove());
@@ -16,39 +18,24 @@ export function makeTable() {
 
     headers.forEach(headerText => {
         let header = document.createElement('th');
-        //header.classList.add(`scope="col"`);
+        header.scope = "col";
+        header.style.textAlign = "center"
         let textNode = document.createTextNode(headerText);
         header.appendChild(textNode);
         headerRow.appendChild(header);
-        console.log("I created a header")
     });
 
-    table.appendChild(headerRow);
+    tableHeader.appendChild(headerRow);
 
     array.forEach(showTime => {
         let row = document.createElement('tr');
         let cell = document.createElement('td');
+        cell.style.textAlign = "center"
         let textNode = document.createTextNode(showTime);
 
         cell.appendChild(textNode);
         row.appendChild(cell);
-        table.appendChild(row);
+        tableBody.appendChild(row);
     });
-
-    /*
-    array.forEach(emp => {
-        let row = document.createElement('tr');
-        Object.values(emp).forEach(text => {
-            console.log(text)
-            let cell = document.createElement('td');
-            console.log(text)
-            let textNode = document.createTextNode(text);
-            console.log(textNode)
-            cell.appendChild(textNode);
-            row.appendChild(cell);
-        })
-        table.appendChild(row);
-    });
-    */
 
 }
