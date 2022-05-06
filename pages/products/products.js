f84cf76169d5015b16335dc108c5e78721d659f8
tobiasvinther <tobi626i@stud.kea.dk>
2022-05-06 10:40:45 +0200

    Hi

--- pages/products/products.js
+++ pages/products/products.js
@@ -1,3 +1,12 @@
+function encode(str) {
+    str = str.replace(/&/g, "&amp;");
+    str = str.replace(/>/g, "&gt;");
+    str = str.replace(/</g, "&lt;");
+    str = str.replace(/"/g, "&quot;");
+    str = str.replace(/'/g, "&#039;");
+    return str;
+}
+
 //const URL = "https://cinema-sem3-backend.azurewebsites.net/api/movies"
 const URL = "http://localhost:8080/api/movies"
 
@@ -16,25 +25,73 @@ export function makeTable() {
     //.then(data => movieList = data)
     .then(fetchedMovies => {
         console.log("hello there")
-        document.getElementById("title-id").innerText = fetchedMovies[0].name
-        document.getElementById("poster-id").src = fetchedMovies[0].posterLink
-
-        const rows = data.map(user => 
-            `
-            <tr>
-              <td>${encode(user.name)}</td>
-              <td>${encode(user.phone)}</td>
-              <td>${encode(user.address.street)}</td>
-              <td>${encode(user.address.city)}</td>
-            </tr>
-            `
-            ).join("\n")
-            document.getElementById("tbl-body").innerHTML = rows
+        //document.getElementById("title-id").innerText = fetchedMovies[0].name
+        //document.getElementById("poster-id").src = fetchedMovies[0].posterLink
 
         fetchedMovies.forEach(movie => {
             let movieTitle = document.createElement('h2')
             document.getElementById("fetch-test-id").appendChild(movieTitle)
             movieTitle.innerText = movie.name
+
+            const createdTable =  
+                `
+                <div class="row">
+                    <h3 style="line-height: 1.0;">${encode(movie.name)}</h3>
+                    <hr>
+                    <div class="col-2">
+                        <img src="${encode(movie.posterLink)}" alt="" width="270" height="">
+                    </div>
+                    <div class="col-1"></div>
+                    <div class="col-9">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col" style="text-align: center; width: 14.28%;">
+                                        <span class="d-none d-xl-block">Mandag</span>
+                                        <span class="date">${encode(dates[0])}</span>
+                                    </th>
+                                    <th scope="col" style="text-align: center; width: 14.28%;">
+                                        <span class="d-none d-xl-block">Tirsdag</span>
+                                        <span class="date">${encode(dates[1])}</span>
+                                    </th>
+                                    <th scope="col" style="text-align: center; width: 14.28%;">
+                                        <span class="d-none d-xl-block">Onsdag</span>
+                                        <span class="date">${encode(dates[2])}</span>
+                                    </th>
+                                    <th scope="col" style="text-align: center; width: 14.28%;">
+                                        <span class="d-none d-xl-block">Torsdag</span>
+                                        <span class="date">${encode(dates[3])}</span>
+                                    </th>
+                                    <th scope="col" style="text-align: center; width: 14.28%;">
+                                        <span class="d-none d-xl-block">Fredag</span>
+                                        <span class="date">${encode(dates[4])}</span>
+                                    </th>
+                                    <th scope="col" style="text-align: center; width: 14.28%;">
+                                        <span class="d-none d-xl-block">Lørdag</span>
+                                        <span class="date">${encode(dates[5])}</span>
+                                    </th>
+                                    <th scope="col" style="text-align: center; width: 14.28%;">
+                                        <span class="d-none d-xl-block">Søndag</span>
+                                        <span class="date">${encode(dates[6])}</span>
+                                    </th>
+                                </tr>
+                                <tbody>
+                                    <tr>
+                                        <td style="text-align: center;">10:00</td>
+                                        <td style="text-align: center;">10:00</td>
+                                    </tr>
+                                    <tr>
+                                        <td style="text-align: center;">12:00</td>
+                                        <td style="text-align: center;">12:00</td>
+                                    </tr>
+                                <tbody>
+                            </thead>
+                        </table>
+                    </div>
+                    </div>
+                `  
+                document.getElementById("tables-container-id").innerHTML = createdTable
+                
 
             /*
             let tablesArea = document.getElementById('tables-area-id')
@@ -63,10 +120,10 @@ export function makeTable() {
 
         })
     })
-
+    /*
     let array = dummyTimes
 
-    /*
+    
     //const table = document.getElementById('movie-table-id')
     const tableHeader = document.getElementById('movie-header-id')
     const tableBody = document.getElementById('movie-body-id')
