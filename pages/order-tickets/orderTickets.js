3228a08f75ddbdfe7e1126e71a4c84761d193076
Thony <thon0101@stud.kea.dk>
2022-05-16 11:27:01 +0200

    You can now order tickets

--- pages/order-tickets/orderTickets.js
+++ pages/order-tickets/orderTickets.js
@@ -1,26 +1,45 @@
-import{SERVER}from "../../settings.js"
+const URL = "http://localhost:8080/api/order-tickets/"
+
+//import{SERVER}from "../../settings.js"
 import {makeOptions} from "../../fetchUtils.js";
 
-const URL = SERVER+"/"
-
-export function addTicketOrder(){
+//const URL = SERVER+"api/order-tickets/"
+let movieID
+export function addTicketOrder(match){
     console.log("test")
-    document.getElementById("btn").onclick = addTicket()
+    document.getElementById("btn-order").onclick = addTicket
+    console.log(JSON.stringify(match?.params?.id))
+    movieID = match?.params?.id
 }
 
-function addTicket(){
+export function loadShowing() {
+    //testArea
+    fetch(window.location.href.toString())
+        .then(res => res.json())
+        .then(fetchedShowing => {
+            console.log("HEllo")
+            console.log(fetchedShowing)
+        })
+    //end testArea
+}
+
+export function addTicket(){
+
     const ticket ={}
-    ticket.firstName = document.getElementById("firstName").value
-    ticket.lastName = document.getElementById("lastName").value
-    ticket.email = document.getElementById("email").value
+    const id = movieID
+    console.log("id is " + movieID)
+    const amountOfTickets = document.getElementById("amountOfTickets").value
+    console.log(ticket)
 
-
-    fetch(URL,makeOptions("POST",ticket,true))
+    fetch(URL+id+"/"+amountOfTickets,makeOptions("PATCH", ticket))
         .then(res=>{
             if(!res.ok){
                 return Promise.reject("Error: " + res.status)
             }
             return res.json()
+        })
+        .then(ticket => {
+            console.log(ticket)
         })
         .catch(e => console.error(e))
 }
